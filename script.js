92de4099a3884c43573678ad87f6a9aa61024cc3
SzabiSch <szabiweb21@gmail.com>
2021-10-07 11:34:45 +0200

    Fehler behoben, denkfehler und fehlender Return zum stop

---
+++ script.js
@@ -0,0 +1,44 @@
+console.log("Hello To Do App");
+
+function addNewTodo() {
+  const newTodoEl = document.querySelector("#new-todo");
+  const todoValue = newTodoEl.value.trim();
+
+  if (todoValue.length === 0) {
+    return;
+  }
+
+  if (isDuplicate(todoValue)) {
+    alert("Try one more time, already exist!");
+    return;
+  }
+
+  const newLi = document.createElement("li");
+  newLi.innerText = todoValue;
+  console.log(todoValue);
+
+  newLi.setAttribute("data-todo", todoValue.toLowerCase());
+
+  const listEl = document.querySelector("#todo-list");
+  listEl.appendChild(newLi);
+
+  newTodoEl.value = "";
+}
+
+const addTodoBtn = document.querySelector("#add-todo");
+addTodoBtn.addEventListener("click", addNewTodo);
+
+function isDuplicate(todo) {
+  todo = todo.toLowerCase();
+  const todoListEl = document.querySelector("#todo-list");
+
+  for (let i = 0; i < todoListEl.children.length; i++) {
+    const currentLiEl = todoListEl.children[i];
+    const currentTodo = currentLiEl.getAttribute("data-todo");
+    if (currentTodo === todo) {
+      return true;
+    }
+  }
+
+  return false;
+}
