da1f7c37663de85504518cc04b445ba1653fedc6
SzabiSch <szabiweb21@gmail.com>
2021-10-19 20:23:41 +0200

    Fixed API GET, API POST, API RELOAD

--- script.js
+++ script.js
@@ -62,3 +62,61 @@ function isChecked(event) {
     checkbox.parentElement.classList.remove("done");
   }
 }
+
+const url = "http://localhost:4730/todos";
+
+renderList();
+
+function renderList() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        const todo = data[i];
+
+        const todoText = todo.description;
+        const liEl = document.createElement("li");
+        liEl.setAttribute("data-id", todo.id);
+
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.id = todo.id;
+
+        const label = document.createElement("label");
+        label.setAttribute("for", todo.id);
+        label.innerText = todoText;
+
+        const listHead = document.querySelector("#todo-list");
+        liEl.append(checkbox, label);
+        listHead.appendChild(liEl);
+      }
+    });
+}
+const listHead = document.querySelector("#todo-list");
+function addNewTodo() {
+  const todoField = document.getElementById("new-todo");
+  const todoText = todoField.value;
+
+  const todoItem = {
+    description: todoText,
+    done: false,
+  };
+  console.log(todoItem);
+  //bis hier gut
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(todoItem),
+  };
+  fetch(url, requestOptions)
+    .then((response) => response.json())
+    .then((data) => console.log("Success:", data))
+    .then(function reloadList() {
+      listHead.innerHTML = "";
+      todoField.value = "";
+
+      setTimeout(renderList, 1500);
+    });
+}
+
+addTodoBtn.addEventListener("click", addNewTodo);
